24cd8f3cf6e41b3979c78d68488e172cbaf52043
Minh Le <minhhlee1711@gmail.com>
2024-10-25 21:38:21 +0700

    create database class and movie dict

---
+++ database.py
@@ -0,0 +1,20 @@
+from movie import Movie
+from movie import movies
+
+class Database:
+    def __init__(self, movie_dict):
+        self.movies = []
+        for movie_data in movies_dict:
+            movie = Movie(movie_data['title'], movie_data['genres'], movie_data['duration'], movie_data['rating'])
+            self.movies.append(movie)
+        
+    def add_movie(self, movie):
+        self.movies.append(movie)
+        
+    def get_movie_by_genres(self, genres):
+        if isinstance(genres, str):
+            genres = [genre.strip() for genre in genres.split(",")]
+
+        return [movie for movie in self.movies 
+                if all(genre in movie.genres for genre in genres)]
+    
