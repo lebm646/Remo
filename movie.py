1bbd1ab4a008a87a0da746f50cbd8be7fd986655
Minh Le <minhhlee1711@gmail.com>
2024-10-25 21:18:55 +0700

    create Movie class

---
+++ movie.py
@@ -0,0 +1,14 @@
+class Movie:
+    def __init__(self, title, genres, duration, rating):
+        self.title = title
+        self.genres = genres #List of genres e.g. ["Action", "Thriller"]
+        self.duration = duration # in minutes
+        self.rating = rating # out of 10
+
+    def __repr__(self):
+        return (
+        f"{self.title}\n"
+        f"Genre: {', '.join(self.genres)}\n"
+        f"Duration: {self.duration} min\n"
+        f"Rating: {self.rating}/10"
+    )
